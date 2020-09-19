bf665377430ace255671f7289c25a52ec17514ae
Harsh Darji <32142226+harshdarji22@users.noreply.github.com>
2020-09-19 16:49:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import flask
-from flask import request, jsonify
+from flask import jsonify
 import urllib.request
 from bs4 import BeautifulSoup
 
@@ -10,6 +10,8 @@ app = flask.Flask(__name__)
 # TODO: add web scraper for formula 2 website.
 base_url = "https://www.fiaformula2.com"
 
+
+# retrieves a list of latest articles
 def get_latest():
     landing_page = base_url + "/Latest"
     print(landing_page)
@@ -32,15 +34,59 @@ def get_latest():
     return result
 
 
+# retrieves all details of the given article
+def get_news(link):
+    with urllib.request.urlopen(link) as respon:
+        response = respon.read()
+
+    soup = BeautifulSoup(response, 'html.parser')
+
+    # text retrieval
+    news = soup.findAll("div", {"class": "rich-text font-text-body"})
+    text = ""
+    for paragraph in news:
+        text = text + paragraph.get_text()
+
+    # image retrieval
+    images = soup.findAll("div", {"class": "f1-image--parent"})
+    img = []
+    for i in images:
+        img.append(i.findAll("img")[0]['data-src'])
+
+    # video retrieval
+    video = soup.findAll("div", {"class": "video-player"})
+    vdo = []
+    for v in video:
+        li = v.findAll("source")[0]['src']
+        t = v.findAll("video")[0]['poster']
+        temp = {'video_thumb': t, 'video_link': li}
+        vdo.append(temp)
+
+    # image gallery retrieval
+    img_g = soup.findAll("div", {"class": "react-images__view-wrapper"})
+    img_gal = []
+    for i in img_g:
+        img = i.findAll("img")[0]['src']
+        img_gal.append(img)
+
+    result = {'text': text, 'images': img, 'videos': vdo, 'img_gallery': img_gal}
+
+    return result
+
+
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>Formula 2 Api</h1>'''
 
 
-# A route to return all of the available entries in our catalog.
 @app.route('/api/v1/latest', methods=['GET'])
 def api_all():
     return jsonify(get_latest())
 
+
+@app.route('/api/v1/news/<path:encoded_url>', methods=['GET'])
+def api_news(encoded_url):
+    link = encoded_url
+    return jsonify(get_news(link))
 
 # app.run()
